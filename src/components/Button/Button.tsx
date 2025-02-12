a2ad2bd25f443085676a94efbaaeabef5a05febc
Aeon Clouzeau <aeonruizc@gmail.com>
2025-02-12 17:02:58 -0600

    add Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Button = () => {
-  return <div></div>;
+const Button = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) => {
+  return (
+    <div>
+      <button className="flex" type="button" onClick={onClick}>
+        {children}
+      </button>
+    </div>
+  );
 };
 
 export default Button;
